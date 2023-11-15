07ac189d61506c4a428e52d869c00e64ecdb0dc0
dinosoupy <anishbasoup@gmail.com>
2023-11-16 00:45:19 +1100

    add datasource interface

--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module github.com/dinosoupy/pod
 go 1.20
 
 require gopkg.in/yaml.v3 v3.0.1
+
+require (
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/segmentio/kafka-go v0.4.44 // indirect
+)
--- pod.go
+++ pod.go
@@ -1,18 +1,18 @@
 package pod
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"os"
+	"time"
 
+	kafka "github.com/segmentio/kafka-go"
 	"gopkg.in/yaml.v3"
 )
 
-type Datasource struct {
-	Name   string            `yaml:"name"`
-	Type   string            `yaml:"type"`
-	URL    string            `yaml:"url"`
-	Fields map[string]string `yaml:"fields"`
+type Datasource interface {
+	Fetch(d time.Duration) ([]byte, error)
 }
 
 type Preprocessor struct {
@@ -33,12 +33,56 @@ type Endpoint struct {
 }
 
 type Pod struct {
-	Name         string       `yaml:"name"`
-	Version      string       `yaml:"version"`
-	Datasource   Datasource   `yaml:"datasource"`
-	Preprocessor Preprocessor `yaml:"preprocessor"`
-	Model        Model        `yaml:"model"`
-	Endpoint     Endpoint     `yaml:"endpoint"`
+	Name         string
+	Version      string
+	Datasource   Datasource
+	Preprocessor Preprocessor
+	Model        Model
+	Endpoint     Endpoint
+}
+
+// Yaml file is unmarshalled into Config before creating a pod
+type Config struct {
+	Name       string `yaml:"name"`
+	Version    string `yaml:"version"`
+	Datasource struct {
+		Name    string   `yaml:"name"`
+		Type    string   `yaml:"type"`
+		Brokers []string `yaml:"brokers"`
+		Topic   string   `yaml:"topic"`
+	} `yaml:"datasource"`
+	Preprocessor struct {
+		Filename string `yaml:"filename"`
+		Type     string `yaml:"type"`
+	} `yaml:"preprocessor"`
+	Model struct {
+		Name     string `yaml:"name"`
+		Filename string `yaml:"filename"`
+	} `yaml:"model"`
+	Endpoint struct {
+		Name   string            `yaml:"name"`
+		Type   string            `yaml:"type"`
+		URL    string            `yaml:"url"`
+		Fields map[string]string `yaml:"fields"`
+	} `yaml:"endpoint"`
+}
+
+// KafkaReader is a wrapper around kafka.Reader that extends
+// a Fetch() method to implement the Datasource interface
+type KafkaReader struct {
+	reader *kafka.Reader
+}
+
+func (k KafkaReader) Fetch(d time.Duration) ([]byte, error) {
+	for {
+		m, err := k.reader.ReadMessage(context.Background())
+		if err != nil {
+			return nil, fmt.Errorf("error: while fetching from kafka topic\n%v", err)
+		}
+		fmt.Printf("message at topic:%v partition:%v offset:%v	%s = %s\n", m.Topic, m.Partition, m.Offset, string(m.Key), string(m.Value))
+
+		time.Sleep(d)
+	}
 }
 
 // New initializes a Pod using the given config filename
@@ -54,10 +98,28 @@ func New(filename string) (*Pod, error) {
 		return nil, fmt.Errorf("error: while reading %s\n%v", filename, err)
 	}
 
-	var pod Pod
-	if err = yaml.Unmarshal(b, &pod); err != nil {
+	var config Config
+	if err = yaml.Unmarshal(b, &config); err != nil {
 		return nil, fmt.Errorf("error: while parsing %s\n%v", filename, err)
 	}
 
-	return &pod, nil
+	var datasource Datasource
+	switch config.Datasource.Type {
+	case "kafka":
+		kr := kafka.NewReader(kafka.ReaderConfig{
+			Brokers:  config.Datasource.Brokers,
+			Topic:    config.Datasource.Topic,
+			MinBytes: 10e3, // 10KB
+			MaxBytes: 10e6, // 10MB
+		})
+		datasource = &KafkaReader{reader: kr}
+	}
+
+	pod := &Pod{
+		Name:       config.Name,
+		Version:    config.Version,
+		Datasource: datasource,
+	}
+
+	return pod, nil
 }
